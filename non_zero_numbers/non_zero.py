1e34d0cbef181f1bb8353c21188190802a3d9842
Marsyus <miadolomboy0917@gmail.com>
2025-06-13 21:03:38 +0800

    Numbers from 0 to 100 except those ending with zero

---
+++ non_zero_numbers/non_zero.py
@@ -0,0 +1,4 @@
+from non_zero_class import NonZero
+
+numbers = NonZero()
+numbers.non_zero()
---
+++ non_zero_numbers/non_zero_class.py
@@ -0,0 +1,5 @@
+class NonZero:
+    def non_zero(self):
+        for num in range(100):
+            if num % 10:
+                print(num)
