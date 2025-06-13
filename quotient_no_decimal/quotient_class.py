1533e6ea16d4e62b92756a04445593c35688d589
Marsyus <miadolomboy0917@gmail.com>
2025-06-13 22:19:54 +0800

    Quotient of two input numbers but without the decimal

---
+++ quotient_no_decimal/quotient_class.py
@@ -0,0 +1,8 @@
+class Quotient:
+    def input_num(self):
+        self.num1 = int(input("Enter first number: "))
+        self.num2 = int(input("Enter second number: "))
+
+    def get_quotient(self):
+        self.quotient = self.num1 // self.num2
+        print(self.quotient)
